899e0f93a05a71c238c82d9dd174d8ce263e758e
ezeutno <ivan.suratno@gmail.com>
2021-03-17 06:22:10 +0000

    Fix dev script on echo

--- dev.sh
+++ dev.sh
@@ -1,16 +1,18 @@
-echo -e "\nInstall Tools"
+echo "Install Tools"
 apt install -y inotify-tools
 
-echo -e "\nBuild on update"
+clear
+
+echo "Build on update"
 
 #!/bin/bash
 i=$((i+1))
 
 while inotifywait -qqre modify "$(pwd)"; 
 do 
-    echo -e "\nStart Building - Number:$i"
+    echo "Start Building - Number:$i"
     docker-compose -f docker-compose.local.yml up --build -d kibana; 
-    echo -e "\nBuild finished - Number:$i"
+    echo "Build finished - Number:$i"
     i=$((i+1))
     sleep 10
 done
