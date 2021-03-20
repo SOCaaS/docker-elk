3eeedb7e8c33e94c998c425532a3591f1c12edac
ezeutno <ivan.suratno@gmail.com>
2021-03-20 11:17:27 +0000

    Add post example to update

--- kibana/plugin/agent_controller/server/routes/index.ts
+++ kibana/plugin/agent_controller/server/routes/index.ts
@@ -1,5 +1,6 @@
 import { IRouter, ILegacyScopedClusterClient } from '../../../../src/core/server';
 import { SearchResponse } from 'elasticsearch';
+import { schema } from '@kbn/config-schema';
 
 export function defineRoutes(router: IRouter) {
   router.get(
@@ -15,7 +16,37 @@ export function defineRoutes(router: IRouter) {
       });
     }
   );
-
+  router.post(
+    {
+        path: '/api/agent_controller/post_example',
+        validate: {
+          body: schema.string()
+        },
+    },
+    async (context, request, response) => {
+        const req = JSON.parse(request.body)
+        const params = {
+          index: req.index,
+          id: req.id,
+          body: {
+            script : {
+              source: "ctx._source.message = params.message",
+              lang: "painless",
+              params : {
+                message : "123"
+              }
+            }
+          }
+        }
+        await context.core.elasticsearch.legacy.client.callAsCurrentUser('update', params);
+        return response.ok({ 
+          body: {
+            message: "okay",
+            response: req
+          }
+         });
+    }
+  );
   router.get(
     {
       path: '/api/agent_controller/default',
