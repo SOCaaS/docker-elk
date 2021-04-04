cea73544893a9eaadbda7425a3211f37176878b2
root <nafuza1299@gmail.com>
2021-04-04 11:45:51 +0000

    euiselect

--- kibana/plugin/agent_controller/public/components/app.tsx
+++ kibana/plugin/agent_controller/public/components/app.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage, I18nProvider } from '@kbn/i18n/react';
-import { BrowserRouter as Router, Route, useHistory} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { createDataStore } from "../data_store/data_store.ts";
-// import { mainSideNav }  from './mainsidenav.tsx';
+import { mainSideNav }  from './mainsidenav.tsx';
 
 import {
     EuiPage,
@@ -320,9 +320,45 @@ export const AgentControllerApp = ({
       );
     });
   };
+  const [InterfaceIsConstructing, SetInterfaceIsConstructing] = useState<boolean | undefined>(true);
 
   const [position, setPosition] = useState("fixed");
+ 
+  const [agentName, setName] = useState<string | undefined>();
+  // Use the core http service to make a response to the server API.
+  const getName = () => {
+    http.get('/api/agent_controller/default').then((res) => {
+      // console.log(res);
+      var name = res["name"];
+      var nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+      setName(nameCapitalized);
+    });
+    return agentName;
+  }
+  
+  const [agentActive, setActive] = useState<boolean | undefined>();
+  const getActive = () => {
+    http.get('/api/agent_controller/default').then((res) => {
+      setActive(res["active"]);
+    });
+    return agentActive;
+  }
 
+  const [agentIP, setIP] = useState<string | undefined>();
+  const getIP = () => {
+    http.get('/api/agent_controller/default').then((res) => {     
+      setIP(res["ip"]);
+    });
+    return agentIP;
+  }
+
+  const [agentCenter, setCenter] = useState<boolean | undefined>();
+  const getCenter = () => {
+    http.get('/api/agent_controller/default').then((res) => {
+      setCenter(res["services"]["suricata"]["rules"][0]["active"]);
+    });
+    return agentCenter;
+  }
   const sections = [
     {
       items: [<EuiHeaderLogo>Agent Controller</EuiHeaderLogo>],
@@ -330,252 +366,194 @@ export const AgentControllerApp = ({
     }
   ];
   //swtich Active
-  const [checkedActive, setCheckedActive] = useState(false);
-  let createSwitchActive = new createSwitch(checkedActive, setCheckedActive);
+  // const [checkedActive, setCheckedActive] = useState(false);
+  let createSwitchActive = new createSwitch(getActive(), setActive);
 
   //switch Rule
-  const [checkedRule, setCheckedRule] = useState(false);
-  let createSwitchRule = new createSwitch(checkedRule, setCheckedRule);
+  // const [checkedRule, setCheckedRule] = useState(false);
+  let createSwitchRule = new createSwitch(getCenter(), setCenter);
 
-  //interface options
-  const options = [
-    { value: "option_one", text: "Option one" },
-    { value: "option_two", text: "Option two" },
-    { value: "option_three", text: "Option three" }
-  ];
-
-  const [value, setValue] = useState(options[1].value);
 
   const onChangeFilter = (e) => {
-    setValue(e.target.value);
+    console.log(e.target.value)
+   // let newArr = [...agentInterface]; // copying the old datas array
+      // newArr[index] = e.target.value; // replace e.target.value with whatever you want to change it to
   };
 
-  const mainSideNav = () => {
-    const [isSideNavOpenOnMobile, setIsSideNavOpenOnMobile] = useState(false);
-    const [selectedItemName, setSelectedItem] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
-    const history = useHistory();
-    const toggleOpenOnMobile = () => {
-      setIsSideNavOpenOnMobile(!isSideNavOpenOnMobile);
-    };
-
-    // function used to select a nav item
-    const selectItem = (name) => {
-      setSelectedItem(name);
-      history.push("/"+name);
-    };
-  
-    // function used to create a nav item
-    const createItem = (name, data = {}, id) => {
-      // NOTE: Duplicate `name` values will cause `id` collisions.
-      return {
-        ...data,
-        id: id,
-        name,
-        isSelected: selectedItemName === id,
-        onClick: () => selectItem(id)
-      };
-    };
-  
-    const sideNav = [
-      createItem('Navigation', {
-        icon: <EuiIcon type="menu" />,
-        items: [
-            createItem('Default', {
-              items: [createItem('TShark', {}, "default/tshark"), createItem('Suricata', {},  "default/suricata")],
-            },"default"),
-          ],
-      }, "nav1"),
-    ];
-    
-    http.get('/api/agent_controller/sidenav_content').then((res) => {
-      for (let x in res){
-        console.log(res[x]["_source"]["name"]);
-        console.log(res[x]["_id"]);
-        let agent_name = res[x]["_source"]["name"];
-        let agent_id = res[x]["_id"];
-        sideNav[0].items.push(
-          createItem(
-            agent_name, {
-            items: [
-              createItem('TShark', {}, agent_id + "/tshark"),
-              createItem('Suricata', {}, agent_id +"/suricata"),
-            ],
-        }, agent_id));
+  const [agentInterface, setInterface] = useState([]);
+  useEffect(() => {
+    http.get('/api/agent_controller/default').then((res) => {
+      let newArr = [];
+      for (let i = 0; i < res["interfaces"].length; i++) {
+        newArr.push({ value: res["interfaces"][i], text: res["interfaces"][i] }); 
       }
-      
-    }).then(() => {setIsLoading(false);});
+      setInterface(newArr);
+    });
+  }, [])
 
-    // for (let i = 1; i < 10; i++) {
-    //   let agent_num = "agentService"+ i.toString();
-    //   sideNav[0].items.push(
-    //     createItem(
-    //       'Agent '+i.toString(), {
-    //       items: [
-    //         createItem('TShark', {}, agent_num + "/tshark"),
-    //         createItem('Suricata', {}, agent_num +"/suricata"),
-    //       ],
-    //   }, agent_num));
-    // }
-  
-    return(
-      <>
-        {!isLoading && (
-          <>
-            <EuiSideNav
-              mobileTitle="Navigate within $APP_NAME"
-              toggleOpenOnMobile={toggleOpenOnMobile}
-              isOpenOnMobile={isSideNavOpenOnMobile}
-              items={sideNav}
-              style={{ width: 192 }}
-            />
-          </>
-        )}
-      </>
-    );
+  const [agentValue, setAgentValue] = useState<string | undefined>();
+  const interfaceValue  = () => {
+    http.get('/api/agent_controller/default').then((res) => {
+      setAgentValue(res["interfaces"][1]);  
+    });
+    return agentValue;
   };
-
+  // if(InterfaceIsConstructing){
+  //   // retrieveData();
+  //   // getName();
+  //   // getCenter();
+  //   // getActive();
+  //   // getIP();
+  //   SetInterfaceIsConstructing(false)
+  // }
 
   // Render the application DOM.
   // Note that `navigation.ui.TopNavMenu` is a stateful component exported on the `navigation` plugin's start contract.
-  return (
-    <Router basename={basename}>
-     <I18nProvider>
-       <>
-         <EuiPage>
-           <EuiPageSideBar sticky><Route path="/" component={mainSideNav} /></EuiPageSideBar>
-           <EuiPageBody>
-             <EuiPageHeader>
-                 <EuiTitle size="l">
-                   <h1>
-                     <FormattedMessage
-                       id="agentController.title"
-                       defaultMessage="{name}"
-                       values={{ name: PLUGIN_NAME }}
-                     />
-                   </h1>
-                 </EuiTitle>
-               </EuiPageHeader>
-             {/* <EuiPanel paddingSize="none" color="transparent"> */}
-             <EuiFlexGroup gutterSize="none" >
-               <EuiFlexItem>
-                 <EuiPanel >
-                   <EuiFlexGroup gutterSize="none" >
-                     <EuiFlexItem>
-                       <EuiSwitch
-                         label="Active"
-                         checked={createSwitchActive.checked}
-                         onChange={(e) => createSwitchActive.onChange(e)}
-                       />
-                     </EuiFlexItem>
-                     <EuiFlexItem>
-                       <EuiText>IP: 192.168.12.0</EuiText>
-                     </EuiFlexItem>
-                   </EuiFlexGroup>
-                 </EuiPanel>
-                 </EuiFlexItem>
-                 <EuiFlexItem>
-                 <EuiPanel >
-                 <EuiText>Status</EuiText>
-                   <EuiFlexGroup gutterSize="none" >
-                     <EuiFlexItem>
-                       <EuiText>CPU:</EuiText>
-                     </EuiFlexItem>
-                     <EuiFlexItem>
-                       <EuiText>Memory:</EuiText>
-                     </EuiFlexItem>
-                   </EuiFlexGroup>
-                 </EuiPanel>
-                 </EuiFlexItem>
-               </EuiFlexGroup>
-                   <EuiFlexGroup gutterSize="none">
-                   <EuiFlexItem>
-                     <EuiPanel>
-                     <EuiFlexGroup gutterSize="none">
-                         <EuiFlexItem>
-                           <EuiText>Interface</EuiText>
-                         </EuiFlexItem>
-                         <EuiFlexItem>
-                           <EuiSelect
-                             id="selectDocExample"
-                             options={options}
-                             value={value}
-                             onChange={(e) => onChangeFilter(e)}
-                             aria-label="Use aria labels when no actual label is in use"
-                           />
-                         </EuiFlexItem>
-                         </EuiFlexGroup>
-                       </EuiPanel>
-                     </EuiFlexItem>
-                   </EuiFlexGroup>
-             {/* </EuiPanel> */}
-             <EuiSpacer/>
-               <EuiFlexGroup gutterSize="none">
-                 <EuiFlexItem>
-                   <EuiTitle>
-                     <h2>Rules</h2>
-                   </EuiTitle>
-                   <EuiSpacer/>
-                   <EuiPanel>
-                   <EuiFlexGroup gutterSize="none">
-                     <EuiFlexItem>
-                       <EuiFlexGroup gutterSize="xs">
-                         <EuiFlexItem>{deleteButton}</EuiFlexItem>
-                         <EuiFlexItem>
-                           <EuiButton
-                             color={"primary"}
-                             onClick={() => {
-                               createModalAdd.showModal();
-                             }}
-                             iconType="plusInCircle"
-                             aria-label="Next"
-                           >
-                             Add Rules
-                           </EuiButton>
-                         </EuiFlexItem>
-                       </EuiFlexGroup>
+  
 
-                       <EuiBasicTable
-                         items={pageOfItems}
-                         itemId="id"
-                         itemIdToExpandedRowMap={itemIdToExpandedRowMap}
-                         isExpandable={true}
-                         hasActions={true}
-                         columns={columns}
-                         pagination={pagination}
-                         sorting={sorting}
-                         isSelectable={true}
-                         selection={selection}
-                         onChange={onTableChange}
-                       />
-                       {modalAdd}
-                       {modalEdit}
-                     </EuiFlexItem>
-                   </EuiFlexGroup>
-                   </EuiPanel>
-                 </EuiFlexItem>
-                 <EuiFlexItem>
-                   <EuiTitle>
-                     <h2>Control Center</h2>
-                   </EuiTitle>
-                   <EuiSpacer/>
-                   <EuiPanel paddingSize="l">
-                     <EuiFlexGroup gutterSize="none">
-                       <EuiFlexItem>
-                         <EuiSwitch
-                           label="Rule DoS"
-                           checked={createSwitchRule.checked}
-                           onChange={(e) => createSwitchRule.onChange(e)}
-                         />
-                       </EuiFlexItem>
-                     </EuiFlexGroup>
-                   </EuiPanel>
-                 </EuiFlexItem>
-               </EuiFlexGroup>
-           </EuiPageBody>
-         </EuiPage>
-       </>
-     </I18nProvider>
-   </Router>
-  );
+    return (
+      <Router basename={basename}>
+      <I18nProvider>
+        <>
+          <EuiPage>
+            <EuiPageSideBar sticky><Route path="/" component={mainSideNav} /></EuiPageSideBar>
+            <EuiPageBody>
+              <EuiPageHeader>
+                  <EuiTitle size="l">
+                    <h1>
+                      <FormattedMessage
+                          id="agentController.getName"
+                          defaultMessage="{name}"
+                          values={{ name: getName() ? getName() : 'Unknown' }}
+                      />
+                    </h1>
+                  </EuiTitle>
+                </EuiPageHeader>
+              {/* <EuiPanel paddingSize="none" color="transparent"> */}
+              <EuiFlexGroup gutterSize="none" >
+                <EuiFlexItem>
+                  <EuiPanel >
+                    <EuiFlexGroup gutterSize="none" >
+                      <EuiFlexItem>
+                        <EuiSwitch
+                          label="Active"
+                          checked={createSwitchActive.checked}
+                          onChange={(e) => createSwitchActive.onChange(e)}
+                        />
+                      </EuiFlexItem>
+                      <EuiFlexItem>  
+                        <EuiText
+                         
+                          id="agentController.getIP"
+                          defaultMessage="{ip}"
+                          values={ {ip: getIP() ? getIP() : 'Unknown'}}
+                        />
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiPanel>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                  <EuiPanel >
+                  <EuiText>Staertus</EuiText>
+                    <EuiFlexGroup gutterSize="none" >
+                      <EuiFlexItem>
+                        <EuiText>CPU:</EuiText>
+                      </EuiFlexItem>
+                      <EuiFlexItem>
+                        <EuiText>Memory:</EuiText>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiPanel>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+                    <EuiFlexGroup gutterSize="none">
+                    <EuiFlexItem>
+                      <EuiPanel>
+                      <EuiFlexGroup gutterSize="none">
+                          <EuiFlexItem>
+                            <EuiText>Interface</EuiText>
+                          </EuiFlexItem>
+                          <EuiFlexItem>
+                            <EuiSelect
+                              id="selectDocExample"
+                              options={agentInterface}
+                              value={interfaceValue()}
+                              onChange={(e) => onChangeFilter(e)}
+                              aria-label="Use aria labels when no actual label is in use"
+                            />
+                          </EuiFlexItem>
+                          </EuiFlexGroup>
+                        </EuiPanel>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+              {/* </EuiPanel> */}
+              <EuiSpacer/>
+                <EuiFlexGroup gutterSize="none">
+                  <EuiFlexItem>
+                    <EuiTitle>
+                      <h2>Rules</h2>
+                    </EuiTitle>
+                    <EuiSpacer/>
+                    <EuiPanel>
+                    <EuiFlexGroup gutterSize="none">
+                      <EuiFlexItem>
+                        <EuiFlexGroup gutterSize="xs">
+                          <EuiFlexItem>{deleteButton}</EuiFlexItem>
+                          <EuiFlexItem>
+                            <EuiButton
+                              color={"primary"}
+                              onClick={() => {
+                                createModalAdd.showModal();
+                              }}
+                              iconType="plusInCircle"
+                              aria-label="Next"
+                            >
+                              Add Rules
+                            </EuiButton>
+                          </EuiFlexItem>
+                        </EuiFlexGroup>
+
+                        <EuiBasicTable
+                          items={pageOfItems}
+                          itemId="id"
+                          itemIdToExpandedRowMap={itemIdToExpandedRowMap}
+                          isExpandable={true}
+                          hasActions={true}
+                          columns={columns}
+                          pagination={pagination}
+                          sorting={sorting}
+                          isSelectable={true}
+                          selection={selection}
+                          onChange={onTableChange}
+                        />
+                        {modalAdd}
+                        {modalEdit}
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiTitle>
+                      <h2>Control Center</h2>
+                    </EuiTitle>
+                    <EuiSpacer/>
+                    <EuiPanel paddingSize="l">
+                      <EuiFlexGroup gutterSize="none">
+                        <EuiFlexItem>
+                          <EuiSwitch
+                            label="Rule DoS"
+                            checked={createSwitchRule.checked}
+                            onChange={(e) => createSwitchRule.onChange(e)}
+                          />
+                        </EuiFlexItem>
+                      </EuiFlexGroup>
+                    </EuiPanel>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+            </EuiPageBody>
+          </EuiPage>
+        </>
+      </I18nProvider>
+    </Router>
+    );
 };
