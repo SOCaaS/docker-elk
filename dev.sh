b4690c2df9601ee71c253641154a32a4feacad0f
ezeutno <ivan.suratno@gmail.com>
2021-04-08 04:31:17 +0000

    Update dev script to new function

--- dev.sh
+++ dev.sh
@@ -11,7 +11,7 @@ docker-compose -f docker-compose.local.yml up --build -d kibana;
 trap ctrl_c INT
 
 function ctrl_c() {
-    docker-compose -f docker-compose.local.yml down
+    docker-compose -f docker-compose.local.yml down -v
 }
 
 clear
