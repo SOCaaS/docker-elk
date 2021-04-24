aa554818df13288306756078e714b6308d9db813
ezeutno <ivan.suratno@gmail.com>
2021-04-24 11:35:45 +0000

    Emojify

--- dev.sh
+++ dev.sh
@@ -1,10 +1,11 @@
-echo "Update"
+echo $'\342\232\231' "Initial Setup" $'\342\232\231'
+echo $'\360\237\223\213' "Update Repository"
 apt update
 
-echo "Install Docker.io & Docker-Compose"
+echo $'\360\237\220\263' "Install Docker.io & Docker-Compose"
 apt install -y docker.io docker-compose
 
-echo "Install Tools"
+echo $'\342\232\231' "Install Tools"
 apt install -y inotify-tools
 docker-compose -f docker-compose.local.yml up --build -d kibana; 
 
@@ -17,12 +18,12 @@ function ctrl_c() {
 
 function check_log() {
     ( docker-compose -f docker-compose.local.yml logs -f kibana & ) | grep -q "http server running at http://0.0.0.0:5601"
-    echo "Server is ready to be tested!"
+    echo $'\360\237\221\223' " Server is ready to be tested!"
 }
 
-echo "Wait for kibana to turn on!"
+echo $'\342\217\261' " Wait for kibana to turn on!"
 check_log
-echo "Creating 5 Agent Service!"
+echo $'\342\254\207' "Creating 5 Agent Service!" $'\342\254\207'
 for a in {1..5}
 do
     curl --location --request POST 'http://localhost:5601/api/agent_controller/create' \
@@ -33,21 +34,22 @@ do
         "name" : "Agent Service '$a'",
         "ip" : "127.0.0.1"
     }'
-    echo "\n"
+    printf "\n"
 done
 
 # clear
 
-echo "Build on update"
+echo $'\342\232\231' "Build on update"
 
 #!/bin/bash
 i=$((i+1))
 
 while inotifywait -qqre modify "$(pwd)"; 
 do 
-    echo "Start Building - Number:$i";
+    echo $'\342\232\231' "Start Building - Number:$i" $'\342\232\231';
     docker-compose -f docker-compose.local.yml up --build -d kibana; 
-    echo "Build finished - Number:$i";
+    echo $'\xF0\x9F\x9A\x80' "Build finished - Number:$i";
+    echo $'\xF0\x9F\x8C\x80' "Git Branch"
     git branch;
     i=$((i+1))
     check_log &
