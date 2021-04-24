0d5979f30b997c2aee50c84b436ebe131fbabe81
ezeutno <ivan.suratno@gmail.com>
2021-04-24 10:06:48 +0000

    add cpu and memory + time update

--- kibana/plugin/agent_controller/server/routes/agentBridgeRoutes.ts
+++ kibana/plugin/agent_controller/server/routes/agentBridgeRoutes.ts
@@ -55,59 +55,101 @@ export function agentBridgeRoutes(router: IRouter) {
 
   router.post(
     {
-      path: '/api/agent_controller/{id}',
+      path: '/api/agent_controller/{id}/usage',
       validate: {
         params: schema.object(
           {
             id: schema.string(),
           }
         ),
+        body: schema.object(
+          {
+              cpu: schema.number(),
+              memory: schema.number()
+          }
+        ),
       }
     },
-    async (context, request, response) => {
-      return response.ok({
+    async (context, request, response) => { 
+
+      const params = {
+        index: "agent-index",
+        id: request.params.id,
         body: {
-          id: request.params.id
+          script : {
+            source: "ctx._source.cpu = params.cpu",
+            lang: "painless",
+            params : {
+              cpu : request.body.cpu
+            }
+          }
         }
-      })
+      }
+      await context.core.elasticsearch.legacy.client.callAsCurrentUser('update', params);
+
+      const mParams = {
+        index: "agent-index",
+        id: request.params.id,
+        body: {
+          script : {
+            source: "ctx._source.memory = params.memory",
+            lang: "painless",
+            params : {
+              memory : request.body.memory
+            }
+          }
+        }
+      }
+      await context.core.elasticsearch.legacy.client.callAsCurrentUser('update', mParams);
+
+      return response.ok({ 
+        body: {
+          message: "okay",
+          response: request.body
+        }
+       });
     }
   );
 
-  // router.post(
-  //   {
-  //       path: '/api/agent_controller/posting_example',
-  //       validate: {
-  //         body: schema.object(
-  //           {
-  //             id: schema.string(),
-  //             index: schema.string(),
-  //             message: schema.string()
-  //           }
-  //         ),
-  //       },
-  //   },
-  //   async (context, request, response) => {
-  //       // const req = JSON.parse(request.body)
-  //       const params = {
-  //         index: request.body.index,
-  //         id: request.body.id,
-  //         body: {
-  //           script : {
-  //             source: "ctx._source.message = params.message",
-  //             lang: "painless",
-  //             params : {
-  //               message : request.body.message
-  //             }
-  //           }
-  //         }
-  //       }
-  //       await context.core.elasticsearch.legacy.client.callAsCurrentUser('update', params);
-  //       return response.ok({ 
-  //         body: {
-  //           message: "okay",
-  //           response: request.body
-  //         }
-  //        });
-  //   }
-  // );
+  router.post(
+    {
+        path: '/api/agent_controller/{id}/time',
+        validate: {
+            params: schema.object(
+                {
+                    id: schema.string(),
+                }
+            ),
+            body: schema.object(
+                {
+                    time: schema.string(),
+                }
+            ),
+        },
+    },
+    async (context, request, response) => {
+
+        const params = {
+          index: "agent-index",
+          id: request.params.id,
+          body: {
+            script : {
+              source: "ctx._source.time = params.time",
+              lang: "painless",
+              params : {
+                time : request.body.time
+              }
+            }
+          }
+        }
+        await context.core.elasticsearch.legacy.client.callAsCurrentUser('update', params);
+        return response.ok({ 
+          body: {
+            message: "okay",
+            response: request.body
+          }
+         });
+    }
+  );
+  
 }
